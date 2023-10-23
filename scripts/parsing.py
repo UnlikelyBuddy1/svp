fae58cad2e205312166ebf78601b072304a06520
Adrianos Sidiras Galante <adrianos.sidiras@gmail.com>
2023-10-23 09:14:35 +0200

    parsing data

--- scripts/parsing.py
+++ scripts/parsing.py
@@ -1,2 +1,7 @@
 import pandas as pd
 import numpy as np
+
+def parse(path):
+        return pd.read_csv(path, sep='\t' , names=["id", "timestamp", "lat", "lon"])
+
+print(parse("original/c3465dad3864bb1e373891fdcfbfcca5f974db6a9e0b646584e07c5f554d7df7"))
