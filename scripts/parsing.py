2ee55515f3738d728e7af082eca98bb7706e3d4b
Adrianos Sidiras Galante <adrianos.sidiras@gmail.com>
2023-10-23 09:26:53 +0200

    reading the csv, making plot

--- scripts/parsing.py
+++ scripts/parsing.py
@@ -1,7 +1,11 @@
 import pandas as pd
 import numpy as np
 
-def parse(path):
-        return pd.read_csv(path, sep='\t' , names=["id", "timestamp", "lat", "lon"])
+ORIGINAL_SET_PATH = "original/c3465dad3864bb1e373891fdcfbfcca5f974db6a9e0b646584e07c5f554d7df7"
 
-print(parse("original/c3465dad3864bb1e373891fdcfbfcca5f974db6a9e0b646584e07c5f554d7df7"))
+def parse(path, n=1000):
+        return pd.read_csv(path, nrows=n, sep='\t' , names=["id", "timestamp", "lat", "lon"])
+
+
+og = parse(ORIGINAL_SET_PATH)
+og['lat'].plot(kind='hist')
