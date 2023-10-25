743c862b2cf905409eef3d4163cc56cd11aef87e
Adrianos Sidiras Galante <adrianos.sidiras@gmail.com>
2023-10-25 16:20:02 +0200

    plotting by week

--- scripts/main.py
+++ scripts/main.py
@@ -1,10 +1,11 @@
 from parsing import *
 from graph import *
+import pandas as pd
 
-ORIGINAL_SET_PATH = "../original/c3465dad3864bb1e373891fdcfbfcca5f974db6a9e0b646584e07c5f554d7df7"
-ANONYMIZED_SET_PATH = "../data/autofill_444_files/S_user_34_c046155c388437412a8755d2f980efb443817bc322e454409e6eeea89714302c"
+# data_frame = parse('../data/original/set', True)
+# data_frame = add_week_number(data_frame)
+# save_per_week(data_frame)
 
-data_frame = parse(ORIGINAL_SET_PATH, 100)
-data_frame = add_week_number(data_frame)
-save_per_week(data_frame)
-#data_frame = parse(ANONYMIZED_SET_PATH, 100)
+# week_frame = pd.read_csv('./output/original/weeks/data_2015_10.csv', sep=',')
+# tiled_week_frame = create_lat_lon_tiles(week_frame)
+# plot_image(tiled_week_frame)
