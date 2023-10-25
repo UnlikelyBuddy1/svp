5269092fa003db7f9abfc4d848e3823eeb3416f2
Notraly <fernandez.coralie2001@gmail.com>
2023-10-25 14:50:58 +0200

    parse: Create function add_week_number

--- scripts/parsing.py
+++ scripts/parsing.py
@@ -1,7 +1,4 @@
-from datetime import datetime
-
 import pandas as pd
-import numpy as np
 
 ORIGINAL_SET_PATH = "../data/original/c3465dad3864bb1e373891fdcfbfcca5f974db6a9e0b646584e07c5f554d7df7"
 ANONYMIZED_SET_PATH = "../data/autofill_444_files/S_user_34_c046155c388437412a8755d2f980efb443817bc322e454409e6eeea89714302c"
@@ -12,22 +9,19 @@ def parse(path, n=100):
     return df[df.id != 'DEL']
 
 
-dSource = parse(ORIGINAL_SET_PATH)
+def add_week_number(path):
+    ds = parse(path)
+    ds['year_week'] = ds['timestamp'].apply(
+        lambda date: str(pd.Timestamp(date).year) + '_' + str(pd.Timestamp(date).weekofyear))
+    ds['timestamp'] = pd.to_datetime(ds['timestamp'], format='%Y-%m-%d %H:%M:%S')
+    ds.sort_values(by='timestamp', inplace=True)
+    return ds
 
-dSource['year_week'] = dSource['timestamp'].apply(
-    lambda date: str(pd.Timestamp(date).year) + '_' + str(pd.Timestamp(date).weekofyear))
-dSource['timestamp'] = pd.to_datetime(dSource['timestamp'], format='%Y-%m-%d %H:%M:%S')
-dSource.sort_values(by='timestamp', inplace=True)
 
+dSource = add_week_number(ORIGINAL_SET_PATH)
 print(dSource.to_markdown() + '\n')
 
-dTarget = parse(ANONYMIZED_SET_PATH)
-
-dTarget['year_week'] = dTarget['timestamp'].apply(
-    lambda date: str(pd.Timestamp(date).year) + '_' + str(pd.Timestamp(date).weekofyear))
-dTarget['timestamp'] = pd.to_datetime(dTarget['timestamp'], format='%Y-%m-%d %H:%M:%S')
-dTarget.sort_values(by='timestamp', inplace=True)
-
+dTarget = add_week_number(ANONYMIZED_SET_PATH)
 print(dTarget.to_markdown() + '\n')
 
 ids_source = pd.unique(dSource["id"])
